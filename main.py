0adb9d888d8cb581bbbada90c34fbc632c93e8c7
JuanPablo Idrovo <89488051+JPIP8@users.noreply.github.com>
2023-04-14 16:39:30 -0400

    Made two separated files instead of 1

--- main.py
+++ main.py
@@ -6,190 +6,11 @@
 
 # Importing libraries:
 import pandas as pd
-from datetime import datetime
-import matplotlib as plt
-import numpy as np
-
-# Loading the rating files:
-df1 = pd.read_csv("ratings/1.csv")
-df2 = pd.read_csv("ratings/2.csv")
-df3 = pd.read_csv("ratings/3.csv")
-df4 = pd.read_csv("ratings/4.csv")
-df5 = pd.read_csv("ratings/5.csv")
-df6 = pd.read_csv("ratings/6.csv")
-df7 = pd.read_csv("ratings/7.csv")
-df8 = pd.read_csv("ratings/8.csv")
-
-# Loading the Platform files:
-dfa = pd.read_csv("amazon_prime_titles.csv")
-dfd = pd.read_csv("disney_plus_titles.csv")
-dfh = pd.read_csv("hulu_titles.csv")
-dfn = pd.read_csv("netflix_titles.csv")
+from fastapi import FastAPI
 
 
-# ####################################################################################################################
-# ########################################                             ###############################################
-# ########################################     Data Transformation     ###############################################
-# ########################################                             ###############################################
-# ####################################################################################################################
-
-### 1. Generate ID field:
-#     Each record's ID should consist of the first letter of the platform name, followed by the show_id already present in the dataset (e.g., "as123" for Amazon titles).
-
-### 2. Fix Nulls:
-#     Replace null values in the "rating" field with the string "G" (which corresponds to a maturity rating of "general for all audiences").
-
-### 3. Normalize Dates:
-#     If present, dates should be in the format "YYYY-mm-dd".
-
-### 4. Normalize Case [camelCase]:
-#     All text fields should be in lower case, without exception.
-
-### 5. Transform the "duration" field:
-#     The "duration" field should be split into two fields: "duration_int", which should be an integer representing the duration, and "duration_type", which should be a string indicating the unit of measurement ("min" for minutes or "season" for TV seasons).
-
-
-# ############################################################################
-# ######################### 1. GENERATE ID FIELDS  ###########################
-# ############################################################################
-
-
-# Merging the rating files into one since they are related:
-# Creating one main data frame: Data Frame Ratings [dfR]
-
-dfR = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8], ignore_index = True)
-
-# Creating a unique ID for the dataframes so, when I merge them, there are not going to be 'show_id' duplicates.
-dfa = dfa.assign(id = 'a' + dfa['show_id'].astype(str))
-dfd = dfd.assign(id = 'd' + dfd['show_id'].astype(str))
-dfh = dfh.assign(id = 'h' + dfh['show_id'].astype(str))
-dfn = dfn.assign(id = 'n' + dfn['show_id'].astype(str))
-
-# Merging the Platform files into one since they are related:
-# Creating one main data frame: Data Frame Platform [dfP]
-
-dfP = pd.concat([dfa, dfd, dfh, dfn], ignore_index = True)
-
-# Sending the 'id' column to be first
-cols = dfP.columns.tolist()
-cols = ['id'] + [col for col in cols if col != 'id']
-dfP = dfP.reindex(columns=cols)
-
-dfP.head(3)
-
-#
-# ############################################################################
-# ############################## 2. FIX NULLS  ###############################
-# ############################################################################
-
-# I want to take a look at the unique values in dfP['rating']
-#print(dfP['rating'].unique())       # After taking a look at 'rating' uniques values, i realized that there where values from 'duration'
-                                    # I will copy the durations to the correct column
-
-
-# Replace null values in 'duration' column with values from 'rating' column
-dfP.loc[dfP['rating'].str.contains(' min', na=False), 'duration'] = dfP['rating']
-
-# Replace remaining null values in 'duration' column with a string
-dfP['duration'] = dfP['duration'].fillna('unknown')
-
-# Replacing NULL values from 'rating' with 'G'
-dfP['rating'] = dfP['rating'].fillna('G')
-
-# Replace the 'duration' values in the 'rating' column with a string
-dfP.loc[dfP['rating'].str.contains(' min', na=False), 'rating'] = 'Unknown'
-
-
-# ############################################################################
-# ########################### 3. NORMALIZE DATES  ############################
-# ############################################################################
-
-# Some dates have white spaces therefore, they give error because they have another format.
-# Stripping the white spaces from the dates:
-dfP['date_added'] = dfP['date_added'].str.strip()
-
-# Now that I don't have white spaces, I can change the format.
-dfP['date_added'] = pd.to_datetime(dfP['date_added'], format='%B %d, %Y').dt.strftime('%Y-%m-%d')
-
-
-
-# ############################################################################
-# ######################### 4. NORMALIZE lowercase  ##########################
-# ############################################################################
-
-dfP['type'] = dfP['type'].str.lower()
-dfP['title'] = dfP['title'].str.lower()
-dfP['director'] = dfP['director'].str.lower()
-dfP['cast'] = dfP['cast'].str.lower()
-dfP['country'] = dfP['country'].str.lower()
-dfP['duration'] = dfP['duration'].str.lower()
-dfP['listed_in'] = dfP['listed_in'].str.lower()
-dfP['description'] = dfP['description'].str.lower()
-
-
-
-# ############################################################################
-# ################### 5. TRANSFORM THE 'DURATION' FIELD  #####################
-# ############################################################################
-
-# Stripping the white spaces from the duration:
-dfP['duration'] = dfP['duration'].str.strip()
-
-# Analyzing the content of the field duration:
-dfP['duration'].unique()
-
-# Creating two new columns
-dfP['duration_int'] = dfP['duration']
-dfP['duration_type'] = dfP['duration']
-
-
-# Setting the 'duration_type' column
-dfP.loc[dfP['duration_type'].str.contains('min', na = False), 'duration_type'] = 'min'
-dfP.loc[dfP['duration_type'].str.contains('season', na = False), 'duration_type'] = 'season'
-
-# Setting the 'duration_int' column
-dfP['duration_int'] = dfP['duration_int'].str.replace(r'[^0-9]', '')
-dfP['duration_int'] = dfP['duration_int'].str.strip()
-
-# Setting the 'duration_int' type as integer
-dfP['duration_int'] = pd.to_numeric(dfP['duration_int'].replace('', '0'))
-dfP['duration_int'] = dfP['duration_int'].astype(int)
-
-# Replacing nulls with 'unknown' in the cast column
-dfP['cast'].fillna('unknown', inplace=True)
-
-
-# ############################################################################
-# #################### 6. TRANSFORMATIONS - BONUS TRACK ######################
-# ############################################################################
-
-# Drop show_id column because now we have id column
-dfP.drop(columns = 'show_id', inplace = True)
-
-# Drop duration column because we splitted that one in two
-dfP.drop(columns = 'duration', inplace = True)
-
-# Replacing some unknown values with 'not rated'.
-dfP['rating'].replace({'UNRATED': 'not rated', 'NOT_RATE': 'not rated', 'NOT RATED': 'not rated', 'Unknown': 'not rated', 'UR': 'not rated', 'NR': 'not rated'}, inplace=True)
-mask = dfP['rating'].str.contains('eason')
-dfP.loc[mask, 'rating'] = 'not rated'
-
-# Replacing some known repeated values with one unified value.
-dfP['rating'].replace({'13+': 'PG-13', 'ALL': 'G', 'ALL_AGES': 'G', 'AGES_18_': 'NC-18', '18+': 'NC-18', '16+': 'NC-16', 'AGES_16_': 'NC-16', '16': 'NC-16', '7+': 'TV-Y7', 'TV-Y7-FV': 'TV-Y7'}, inplace=True)
-
-
-# Change the name of rating to score
-dfR = dfR.rename(columns={'rating': 'score'})
-
-# I will GROUP BY 'movieId' and i'll take the mean from the ratings
-dfR_gouped = dfR.groupby('movieId', as_index = False)   # Grouping while maintaining the original index
-score = dfR_gouped.score.mean().round(1)               # Taking the mean of the grouped df and rounding it with one decimal
-
-# Changing the name of "movieId" to 'id' so, it matches with the other dataFrame.
-score = score.rename(columns = {'movieId' : 'id'})
-
-# Merging the dfP [Platform] and dfR [Ratings] dataFrames with their id.
-df_score = pd.merge(dfP, score, on = 'id', how = 'outer')
+# Loading the files:
+df = pd.read_csv("data.csv")
 
 
 # ####################################################################################################################
@@ -213,23 +34,9 @@ df_score = pd.merge(dfP, score, on = 'id', how = 'outer')
 
 # 6. The total quantity of content/products (everything available on streaming, series, documentaries, movies, etc.) according to the given audience rating (for which audience was the movie classified). The function must be called get_contents(rating) and must return the total number of content with that audience rating.
 
-###   API TEST   ###
 
-# 1. Importing FASTAPI
-from fastapi import FastAPI
-
-# 2. Creating a FastAPI instance
+# Creating a FastAPI instance
 app = FastAPI()
-
-# 3. Defining a path operation decorato@app.get("/")
-
-# 4. Define the path operation function
-async def root():
-
-# 5. Returning the content
-    return {"message": "Hello World"}
-
-
 
 # ############################################################################
 # ########################## 1. get_max_duration  ############################
@@ -238,7 +45,7 @@ async def root():
 # The function must be called get_max_duration(year, platform, duration_type) and must return only the string of the movie name.
 
 @app.get("/get_max_duration/{year}/{platform}/{duration_type}")
-async def get_max_duration(year: int, platform: str, duration_type: str):
+def get_max_duration(year: int, platform: str, duration_type: str):
 # def get_max_duration(year: int, platform: str, duration_type: str):
     # Setting variables to lowercase
     platform = platform.lower()
@@ -282,7 +89,7 @@ async def get_max_duration(year: int, platform: str, duration_type: str):
      
 
     # Creating a filter for the different platforms that the user might set as an input.
-    maskPlatform = dfP[dfP['id'].str.contains(p, na = False)].copy()
+    maskPlatform = df[df['id'].str.contains(p, na = False)].copy()
 
     # Creating a filter for the different duration types that the user might set as an input.
     mask_P_D= maskPlatform[maskPlatform['duration_type'].str.contains(d, na = False)]
@@ -301,11 +108,11 @@ async def get_max_duration(year: int, platform: str, duration_type: str):
     movieName = mask['title'].to_string(index = False)
 
     # Returning the title of the movie.
-    return {"title": movieName}
+    return {"movieName": movieName}
 
 
 test = get_max_duration(2014, 'amazon', 'min')
-
+print(test)
 
 
 # ############################################################################
@@ -316,7 +123,7 @@ test = get_max_duration(2014, 'amazon', 'min')
 # The function must be called get_score_count(platform, scored, year) and must return an int, with the total number of movies that meet the requested criteria.
 
 @app.get("/get_score_count/{platform}/{scored}/{year}")
-async def get_score_count(platform: str, scored: float, year: int):
+def get_score_count(platform: str, scored: float, year: int):
 # def get_score_count(platform: str, scored: float, year: int):
 
     # Error proof for user input - platform
@@ -361,7 +168,7 @@ async def get_score_count(platform: str, scored: float, year: int):
 
 
     # Filter by type - only movies
-    mask = df_score[df_score['type'].str.contains('movie', na = False)].copy()
+    mask = df[df['type'].str.contains('movie', na = False)].copy()
 
     # Filter by platform
     mask_platform = mask[mask['id'].str.contains(p, na = False)]
@@ -380,9 +187,14 @@ async def get_score_count(platform: str, scored: float, year: int):
     # df_outer.info()
     mask_score_platform_year.head(3)
 
-    return {"get_score_count" : get_score_count}
+    return {
+        "platform" : platform,
+        "quantity" : get_score_count,
+        "year" : year,
+        "score" : scored
+        }
 
-get_score_count('amaZon', 3.6, 2014)
+print(get_score_count('amaZon', 3.6, 2014))
 
 
 
@@ -395,7 +207,7 @@ get_score_count('amaZon', 3.6, 2014)
 # The platforms must be named Amazon, Netflix, Hulu, Disney.
 
 @app.get("/get_count_platform/{platform}")
-async def get_count_platform(platform: str):
+def get_count_platform(platform: str):
 # def get_count_platform(platform: str):
     
     # This is an if statement for the user input variable [platform] transformation and for catching some potential errors.
@@ -413,7 +225,7 @@ async def get_count_platform(platform: str):
         return somethingIsWrong
 
     # Creating a filter for the different platforms that the user might set as an input.
-    mask_plt_3 = dfP[dfP['id'].str.contains(plt, na = False)].copy()
+    mask_plt_3 = df[df['id'].str.contains(plt, na = False)].copy()
 
     mask_movie_3 = mask_plt_3[mask_plt_3['type'].str.contains('movie', na = False)]
 
@@ -422,10 +234,12 @@ async def get_count_platform(platform: str):
     num_movies = int(num_movies)
 
     
-    return {"num_movies": num_movies}
-    # title - a9668! - n8807! - d1450! - h3073!
+    return {
+        "platform" : platform,
+        "movies" : num_movies
+        }
     
-get_count_platform('az')
+print(get_count_platform('az'))
 
 
 
@@ -440,7 +254,7 @@ get_count_platform('az')
 
 
 @app.get("/get_actor/{platform}/{year}")
-async def get_actor(platform: str, year: int):
+def get_actor(platform: str, year: int):
 # def get_actor(platform: str, year: int):
     
     # This is an if statement for the user input variable [platform] transformation and for catching some potential errors.
@@ -468,7 +282,7 @@ async def get_actor(platform: str, year: int):
         y = year
     
     # Creating a filter for the different platforms that the user might set as an input.
-    mask_plt_4 = dfP[dfP['id'].str.contains(plt, na = False)].copy()
+    mask_plt_4 = df[df['id'].str.contains(plt, na = False)].copy()
 
     # Creating a filter for the different year that the user might set as an input.
     mask_year_4 = mask_plt_4[mask_plt_4['release_year'] == y]
@@ -494,15 +308,17 @@ async def get_actor(platform: str, year: int):
     sorted_dict = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
 
     # Taking the second value because the first value is "unknown" [they were nulls]
-    second_max_key = sorted_dict[1][0] # Name of actor
-    second_max_value = sorted_dict[1][1] # Number of times the actor appears
+    actorName = sorted_dict[1][0] # Name of actor
+    frequency = sorted_dict[1][1] # Number of times the actor appears
 
-    actorName = second_max_key
+    return {
+        "platform" : platform,
+        "year" : year,
+        "actor" : actorName,
+        "frequency" : frequency}
 
-    return {'actorName': actorName}
 
-
-get_actor('amazon', 2014)
+print(get_actor('amazon', 2014))
 
 
 
@@ -515,7 +331,7 @@ get_actor('amazon', 2014)
 # by country and year in a dictionary with the variables named 'country' (name of the country), 'year' (year), 'movie' (type of content).
 
 @app.get("/prod_per_country/{types}/{country}/{year}")
-async def prod_per_country(types: str, country: str, year: int):
+def prod_per_country(types: str, country: str, year: int):
 # def prod_per_country(types: str, country: str, year: int):
 
     # Error proof for user input - country
@@ -548,7 +364,7 @@ async def prod_per_country(types: str, country: str, year: int):
         y = year
 
     # Filtering by the user input - country
-    mask_country = dfP[dfP['country'].str.contains(c, na = False)].copy()
+    mask_country = df[df['country'].str.contains(c, na = False)].copy()
 
     # Filtering by the user input - type
     mask_country_type = mask_country[mask_country['type'].str.contains(t, na = False)]
@@ -558,11 +374,19 @@ async def prod_per_country(types: str, country: str, year: int):
 
     # mask_country_type_year = mask_country_type_year.type.country.release_year
 
-    prod_per_country = mask_country_type_year[['type', 'country', 'release_year']].reset_index(drop=True).to_dict('index')
+    # prod_per_country = mask_country_type_year[['type', 'country', 'release_year']].reset_index(drop=True).to_dict('index')
 
-    return {"prod_per_country" : prod_per_country}
+    prod_per_country = mask_country_type_year[['type', 'country', 'release_year']].reset_index(drop=True)
+    count_type = prod_per_country.type.count()
 
-prod_per_country('movie', 'indi', 2021)
+
+    print(prod_per_country.head())
+    return {
+        "country" : prod_per_country.country[0],
+        "year" : year,
+        "movies" : count_type}
+
+print(prod_per_country('movie', 'indi', 2021))
 
 
 # ############################################################################
@@ -574,20 +398,22 @@ prod_per_country('movie', 'indi', 2021)
 # The function must be called get_contents(rating) and must return the total number of content with that audience rating.
 
 @app.get("/get_contents/{rating}")
-async def get_contents(rating: str):
+def get_contents(rating: str):
 
 # def get_contents(rating: str):
 
     # Creating a filter for the different RATING that the user might set as an input.
-    mask_rating = dfP[dfP['rating'].str.contains(rating, na = False)].copy()
+    mask_rating = df[df['rating'].str.contains(rating, na = False)].copy()
 
     # Making a count for the different titles of movies
     num_content = mask_rating['title'].count()
     get_contents = int(num_content)
 
-    return {"get_contents" : get_contents}
+    return {
+        "rating" : rating,
+        "contents" : get_contents}
 
-get_contents('G')
+print(get_contents('g'))
 
 
 
